3f2fc999e46bf9c417bc6274c673e38437a4fef7
Evaldas Grublys <evaldas.grublys@mif.stud.vu.lt>
2020-09-22 19:59:35 +0300

    WPF UI update.

--- ComparisonShoppingEngine/MainWindow.xaml.cs
+++ ComparisonShoppingEngine/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Printing;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +25,41 @@ namespace ComparisonShoppingEngine
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void ButtonLogout_Click(object sender, RoutedEventArgs e)
+        {
+            // Future feature: confirm logout operation and close program
+
+            // Close program
+            Application.Current.Shutdown();
+        }
+
+        private void HeaderGrid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // Method to drag window left click drag on Header panel
+            DragMove();
+        }
+
+        private void SearchTest_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Check if search input it not empty and Enter key is pressed
+            if (Key.Enter == e.Key && !SearchText.Equals(""))
+            {
+                // Display text in content grid
+                txt.Text = "Paieška atlikta!"+"\nGauti duomenys: "+SearchText.Text;
+                SearchText.Text = "Search...";
+            }
+        }
+
+        private void Help_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            // "Need any help?" Label pressed
+        }
+
+        private void NewShoppingList_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
         }
     }
 }
