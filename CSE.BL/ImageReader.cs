278d44b5e5075493fbf95181ef0069150b4b0cdd
kkersis <kestu.kersis@gmail.com>
2020-09-24 12:13:39 +0300

    created simple OCR system

---
+++ CSE.BL/ImageReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Tesseract;
+using System.Drawing;
+
+namespace CSE.BL
+{
+    public class ImageReader
+    {
+        public string ReadImage(string imgName)
+        {
+            Pix img = Pix.LoadFromFile(imgName);       // image must be copied to output dir (on image properties -> copy to output dir = true)
+                                                       // ofc for phone app other solution will be found
+            TesseractEngine engine = new TesseractEngine("./tessdata", "lit", EngineMode.Default);
+            Page page = engine.Process(img, PageSegMode.Auto);
+            string text = page.GetText();
+
+            return text;
+        }
+    }
+}
