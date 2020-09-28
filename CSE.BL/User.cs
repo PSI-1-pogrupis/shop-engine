a6f4fd4fc2036a022b72bda74933498a3d707705
Dominykas Jasiūnas <dominykasjsns@gmail.com>
2020-09-28 17:13:17 +0300

    User and UserValidator classes added.

---
+++ CSE.BL/User.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE.BL
+{
+    public class User
+    {
+        public string emailAddress { get; private set; }
+        public string username { get; private set; }
+    }
+}
