e821e2ab5c00366079e826c4774e3e1b0d2a1dc4
Dominykas Jasiūnas <dominykasjsns@gmail.com>
2020-10-05 10:36:25 +0300

    Added a popup for selecting the amount of items to add.

--- ViewModels/NewShoppingListViewModel.cs
+++ ViewModels/NewShoppingListViewModel.cs
@@ -6,17 +6,21 @@ using System.Text;
 using System.Windows.Input;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
 
 namespace ViewModels
 {
     public class NewShoppingListViewModel : BaseViewModel
     {
         private string searchText = "";
-
         private readonly List<ShoppingItem> Products;
 
         private ObservableCollection<ShoppingItem> shoppingList;
         private List<ShoppingItem> productList;
+
+        private bool isPopupVisible = false;
+        private ShoppingItem selectedItem;
+        private int selectedAmount = 1;
 
         public ObservableCollection<ShoppingItem> ShoppingList
         {
@@ -49,17 +53,49 @@ namespace ViewModels
             }
         }
 
+        public ShoppingItem SelectedItem{
+            get { return selectedItem; }
+            set
+            {
+                selectedItem = value;
+                OnPropertyChanged(nameof(SelectedItem));
+            }
+        }
+
+        public string SelectedAmount
+        {
+            get { return selectedAmount.ToString(); }
+            set
+            {
+                if (int.TryParse(value, out int amount)) selectedAmount = amount;
+                else selectedAmount = 1;
+
+                OnPropertyChanged(nameof(SelectedAmount));
+            }
+        }
+
+        public bool IsPopupVisible
+        {
+            get { return isPopupVisible; }
+            set
+            {
+                isPopupVisible = value;
+                OnPropertyChanged(nameof(IsPopupVisible));
+            }
+        }
+
+        public ICommand ClosePopupCommand { get; private set; }
+        public ICommand AddItemPopupCommand { get; private set; }
         public ICommand AddItemCommand { get; private set; }
-
         public ICommand RemoveItemCommand { get; private set; }
-
         public ICommand SearchCommand { get; private set; }
-
         public NewShoppingListViewModel()
         {
             AddItemCommand = new RelayCommand(AddItem, canExecute => true);
             RemoveItemCommand = new RelayCommand(RemoveItem, canExecute => true);
             SearchCommand = new RelayCommand(SearchList, canExecute => true);
+            AddItemPopupCommand = new RelayCommand(AddPopup, canExecute => true);
+            ClosePopupCommand = new RelayCommand(ClosePopup, canExecute => true);
 
             ProductList = new List<ShoppingItem>();
             ShoppingList = new ObservableCollection<ShoppingItem>();
@@ -74,24 +110,35 @@ namespace ViewModels
             productList = Products;
         }
 
-        private void AddItem(object parameter)
+        private void AddPopup(object parameter)
         {
-            if (parameter is ShoppingItem selectedItem)
+            if(parameter is ShoppingItem selected)
             {
-                foreach(ShoppingItem item in ShoppingList)
-                {
-                    if (item.Name.Equals(selectedItem.Name))
-                    {
-                        ShoppingList.Remove(item);
-                        item.Amount += 1;
-                        ShoppingList.Add(item);
-
-                        return;
-                    }
-                }
-
-                ShoppingList.Add(new ShoppingItem(selectedItem));
+                SelectedItem = selected;
+                IsPopupVisible = true;
             }
+        }
+
+        private void AddItem(object parameter)
+        {   
+            foreach(ShoppingItem item in ShoppingList)
+            {
+                if (item.Name.Equals(SelectedItem.Name))
+                {
+                    ShoppingList.Remove(item);
+                    item.Amount += selectedAmount;
+                    ShoppingList.Add(item);
+
+                    ClosePopup(null);
+                    return;
+                }
+            }
+
+            ShoppingItem newItem = (new ShoppingItem(SelectedItem));
+            newItem.Amount = selectedAmount;
+            ShoppingList.Add(newItem);
+
+            ClosePopup(null);
         }
 
         private void RemoveItem(object parameter)
@@ -99,25 +146,27 @@ namespace ViewModels
             if (parameter is ShoppingItem selectedItem)
             {
                 ShoppingList.Remove(selectedItem);
-
-                if (selectedItem.Amount - 1 > 0)
-                {
-                    selectedItem.Amount -= 1;
-                    ShoppingList.Add(selectedItem);
-                }
             }
         }
 
         private void SearchList(object parameter)
         {
-            
+
             string text = SearchText;
 
             var selected = from i in Products
                            where i.Name.Contains(text)
                            select i;
 
-            ProductList = selected.ToList(); 
+            ProductList = selected.ToList();
         }
+
+        private void ClosePopup(object parameter)
+        {
+            SelectedAmount = "1";
+            SelectedItem = null;
+            IsPopupVisible = false;
+        }
+        
     }
 }
