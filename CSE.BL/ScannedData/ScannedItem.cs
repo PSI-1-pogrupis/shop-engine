42c15dd96a6e6734334667c78abe871c843eb4bb
Gintarebalsyte <gintarebalsyte00@gmail.com>
2020-10-05 12:21:50 +0300

    added ScannedItem class

---
+++ CSE.BL/ScannedData/ScannedItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE.BL.ScannedData
+{
+    [Serializable]
+    class ScannedItem
+    {
+        private string name;
+        private float price;
+
+        public string Name 
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                    name = value;
+            }
+        }
+        public float Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public ScannedItem (string name, float price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+    }
+}
