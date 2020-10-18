ef10b45d50a07e579105296d60dad8bc060bea6f
Evaldas Grublys <evaldas.grublys@mif.stud.vu.lt>
2020-10-18 13:03:39 +0300

    Shopping Item interface implementation.

---
+++ CSE.BL/Interfaces/IShoppingItem.cs
@@ -0,0 +1,14 @@
+﻿using CSE.BL.ShoppingList;
+
+namespace CSE.BL.Interfaces
+{
+    public interface IShoppingItem
+    {
+        int? Id { get; set; }
+        ShopTypes Shop { get; set; }
+        string Name { get; set; }
+        double Amount { get; set; }
+        UnitTypes Unit { get; set; }
+        double Price { get; set; }
+    }
+}
