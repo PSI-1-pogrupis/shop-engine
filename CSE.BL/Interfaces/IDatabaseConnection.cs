5ebb72a8e915e84c32ce9ca9a8b4454af86c5c8c
Evaldas Grublys <evaldas.grublys@mif.stud.vu.lt>
2020-10-18 13:02:47 +0300

    BinaryFileManager class update and DatabaseConnection interface implementation.

---
+++ CSE.BL/Interfaces/IDatabaseConnection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSE.BL.Interfaces
+{
+    public interface IDatabaseConnection
+    {
+        void SetConnection(object dataPath);
+        void Dispose();
+    }
+}
