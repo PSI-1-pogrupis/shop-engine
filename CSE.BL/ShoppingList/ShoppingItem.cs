90cba3eedd8654fc4e90eb8c3fc303b298f08666
Evaldas Grublys <evaldas.grublys@mif.stud.vu.lt>
2020-10-18 13:20:26 +0300

    Implemented new fields and methods for ShoppingItem entity

--- CSE.BL/ShoppingList/ShoppingItem.cs
+++ CSE.BL/ShoppingList/ShoppingItem.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSE.BL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,14 +9,42 @@ namespace CSE.BL.ShoppingList
      * the name, amount, and measurement unit type*/
 
     [Serializable]
-    public class ShoppingItem
+    public class ShoppingItem : IShoppingItem
     {
+        private int? id;//item id
+        private ShopTypes shop; //item shop place
         private string name; //name of the item
-        private double amount; //amount of the item 
         private UnitTypes unit; //the unit of measurement for this item
+        private double amount; //amount of the item 
+        private double price; //price of the item
+
+        public int? Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        // get/set index of shop
+        public ShopTypes Shop
+        {
+            get
+            {
+                return shop;
+            }
+            set
+            {
+                shop = value;
+            }
+        }
 
         // get/set the name of the item
-        public string Name 
+        public string Name
         {
             get
             {
@@ -58,19 +87,61 @@ namespace CSE.BL.ShoppingList
             }
         }
 
-        //constructor for the ShoppingItem class
-        public ShoppingItem (string name, double amount, UnitTypes unit)
+        // get/set the price of the item
+        public double Price
         {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    price = value;
+                }
+            }
+        }
+
+        public ShoppingItem()
+        {
+        }
+
+        //constructor for the ShoppingItem class
+        public ShoppingItem(int? id, ShopTypes shop, string name, double amount, UnitTypes unit, double price)
+        {
+            this.id = id;
+            this.shop = shop;
             this.name = name;
             this.amount = amount;
             this.unit = unit;
+            this.price = price;
+        }
+
+        public ShoppingItem(IShoppingItem item)
+        {
+            if (item != null)
+            {
+                id = item.Id;
+                shop = item.Shop;
+                name = item.Name;
+                amount = item.Amount;
+                unit = item.Unit;
+                price = item.Price;
+            }
         }
 
         public ShoppingItem(ShoppingItem item)
         {
-            name = item.name;
-            amount = item.amount;
-            unit = item.unit;
+            if (item != null)
+            {
+                id = item.id;
+                shop = item.shop;
+                name = item.name;
+                amount = item.amount;
+                unit = item.unit;
+                price = item.price;
+            }
         }
 
         public override string ToString()
