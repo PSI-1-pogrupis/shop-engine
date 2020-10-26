ff29c2f0f4d3daf3a8ab70c4608e607e3f09c1d9
Evaldas Grublys <evaldas.grublys@mif.stud.vu.lt>
2020-10-26 12:23:44 +0200

    Added database models

---
+++ CSE.BL/Database/Models/Maxima.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSE.BL.Database.Models
+{
+    public partial class Maxima
+    {
+        public int Id { get; set; }
+        public string Product { get; set; }
+        public double Price { get; set; }
+        public DateTime? Date { get; set; }
+    }
+}
