4979810c933ec1fe830766a0762aa9682d7438a1
Dominykas Jasiūnas <dominykasjsns@gmail.com>
2020-11-14 15:17:20 +0200

    Added distance between strings algorithm to string extensions class.

--- CSE.BL/StringExtensions.cs
+++ CSE.BL/StringExtensions.cs
@@ -27,5 +27,42 @@ namespace CSE.BL
 
             return count;
         }
+
+        /*
+         * Levenshtein distance algorithm.
+         * Finds the distance (number of single character edits required to transform one string into another) between two strings.
+         */
+        public static int DistanceTo(this string source, string other)
+        {
+            int sourceLength = source.Length;
+            int otherLength = other.Length;
+            int[,] distance = new int[sourceLength + 1, otherLength + 1];
+
+            if (sourceLength == 0)
+            {
+                return otherLength;
+            }
+
+            if (otherLength == 0)
+            {
+                return sourceLength;
+            }
+
+            for (int i = 0; i <= sourceLength; distance[i, 0] = i++) { };
+
+            for (int j = 0; j <= otherLength; distance[0, j] = j++) { };
+
+            for (int i = 1; i <= sourceLength; i++)
+            {
+                for (int j = 1; j <= otherLength; j++)
+                {
+                    int cost = (other[j - 1] == source[i - 1]) ? 0 : 1;
+
+                    distance[i, j] = Math.Min(Math.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1), distance[i - 1, j - 1] + cost);
+                }
+            }
+
+            return distance[sourceLength, otherLength];
+        }
     }
 }
