db5786ebaf630baef5cdc9686b6302e8e8e39716
Evaldas Grublys <evaldas.grublys@mif.stud.vu.lt>
2020-11-22 21:14:28 +0200

    Implemented user Receipt model

---
+++ ComparisonShoppingEngineAPI/Data/Models/Receipt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComparisonShoppingEngineAPI.Data.Models
+{
+    public class Receipt
+    {
+        public Receipt()
+        {
+            ReceiptProducts = new HashSet<ReceiptProduct>();
+        }
+        [Key]
+        public int ReceiptId { get; set; }
+        public int UserId { get; set; }
+        public int ShopId { get; set; }
+        public decimal Total { get; set; }
+        public DateTime? Date { get; set; }
+
+        public virtual ICollection<ReceiptProduct> ReceiptProducts { get; set; }
+        public virtual Shop Shop { get; set; }
+    }
+}
