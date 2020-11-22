a4866c5e789ac238e50c4c19e8ff163307ff0a44
Evaldas Grublys <evaldas.grublys@mif.stud.vu.lt>
2020-11-22 21:10:11 +0200

    Implemented User model

---
+++ ComparisonShoppingEngineAPI/Data/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComparisonShoppingEngineAPI.Data.Models
+{
+    public class User
+    {
+        public User()
+        {
+            Receipt = new HashSet<Receipt>();
+        }
+
+        [Key]
+        public int UserId { get; set; }
+        public string UserEmail { get; set; }
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public virtual ICollection<Receipt> Receipt { get; set; }
+    }
+}
