186d3601ca7cda2b749187a21b38d745f51dca78
Evaldas Grublys <evaldas.grublys@mif.stud.vu.lt>
2020-11-22 21:22:18 +0200

    Implemented Receipt product model

---
+++ ComparisonShoppingEngineAPI/Data/Models/ReceiptProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComparisonShoppingEngineAPI.Data.Models
+{
+    public class ReceiptProduct
+    {
+        [Key]
+        public int ReceiptProductId { get; set; }
+        public int ReceiptId { get; set; }
+        public int ProductId { get; set; }
+        public decimal ProductPrice { get; set; }
+        public virtual Product Product { get; set; }
+    }
+}
--- ComparisonShoppingEngineAPI/Data/Models/User.cs
+++ ComparisonShoppingEngineAPI/Data/Models/User.cs
@@ -10,7 +10,7 @@ namespace ComparisonShoppingEngineAPI.Data.Models
     {
         public User()
         {
-            Receipt = new HashSet<Receipt>();
+            Receipts = new HashSet<Receipt>();
         }
 
         [Key]
@@ -19,6 +19,6 @@ namespace ComparisonShoppingEngineAPI.Data.Models
         public string UserName { get; set; }
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
-        public virtual ICollection<Receipt> Receipt { get; set; }
+        public virtual ICollection<Receipt> Receipts { get; set; }
     }
 }
