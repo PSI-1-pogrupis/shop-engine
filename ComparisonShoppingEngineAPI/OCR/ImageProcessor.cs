658d7214a511d0ef559bce7ecf32e54aedaf73ee
kkersis <kestu.kersis@gmail.com>
2020-11-26 12:11:19 +0200

    removed density change line in ocr

--- ComparisonShoppingEngineAPI/OCR/ImageProcessor.cs
+++ ComparisonShoppingEngineAPI/OCR/ImageProcessor.cs
@@ -11,7 +11,7 @@ namespace ComparisonShoppingEngineAPI
         public MagickImage ProcessImage(MagickImage img)
         {
             img.Resize((int)(1.5f * img.Width), (int)(1.5f * img.Height));
-            img.Density = new Density(300f);
+            //img.Density = new Density(300f);
             TextCleanerScript cleaner = new TextCleanerScript();
             img.Deskew(new Percentage(80));
             
